1a152cf861f26b58bc86ce6fa1898a56b24d2626
pokakaite <danilova.katherine02@gmail.com>
2024-05-10 12:53:09 +0500

    в процессе

---
+++ На проверку/PyHW29_2/Задание_3/model.py
@@ -0,0 +1,28 @@
+class Book:
+    pass
+
+
+class AnnaKarenina:
+    def __init__(self):
+        self.name = 'Анна Каренина'
+        self.year = 1877
+        self.author = 'Лев Николаевич Толстой'
+
+
+class MasterAndMargarita:
+    def __init__(self):
+        self.name = 'Мастер и Маргарита'
+        self.year = 1940
+        self.author = 'Михаил Афанасьевич Булгаков'
+
+
+class Reader:
+    def __init__(self):
+        self.name = 'Данилова Екатерина Александровна'
+        self.age = 21
+
+
+class Librarian:
+    def __init__(self):
+        self.name = 'Библиотекарь'
+        self.salary = 20000
