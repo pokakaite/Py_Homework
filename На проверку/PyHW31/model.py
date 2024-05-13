73464a9582dce4d0f7a8832e7f36d0aad107a5ef
pokakaite <danilova.katherine02@gmail.com>
2024-05-13 21:20:43 +0500

    дз

--- На проверку/PyHW31/model.py
+++ На проверку/PyHW31/model.py
@@ -1,10 +1,158 @@
 class HotDog:
+    def __init__(self, name, bun, sausage, sause, topping):
+        self.name = name
+        self.bun = bun
+        self.sausage = sausage
+        self.sause = sause
+        self.topping = topping
+
+    def get_hot_dog(self):
+        hot_dog = {
+            'Название': self.name,
+            'Булочка': self.bun,
+            'Сосиска': self.sausage,
+            'Соус': self.sause,
+            'Добавка': self.topping
+        }
+        return hot_dog
+
+
+class RecipeName:
+    names = []
+
     def __init__(self):
         self.name = None
-        self.bun = None
-        self.sausage = None
 
-    def __repr__(self):
-        return ('\nНазвание хот-дога - {0.name}\n'
-                'Булочка - {0.bun}\n'
-                'Сосиска - {0.sausage}\n').format(self)
+    def get_name(self):
+        return self.name
+
+    def add_to_names(self):
+        RecipeName.names.append(self.name)
+
+
+class Standart(RecipeName):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Стандартный'
+
+
+class Mexican(RecipeName):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Мексиканский'
+
+
+class Vegan(RecipeName):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Веганский'
+
+
+class Bun:
+    buns = []
+    def __init__(self):
+        self.name = None
+
+    def get_name(self):
+        return self.name
+
+    def add_to_buns(self):
+        Bun.buns.append(self.name)
+
+
+class Wheat(Bun):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Пшеничная'
+
+
+class Rye(Bun):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Цельнозерновая ржаная мука'
+
+class Sausage:
+    sausages = []
+
+    def __init__(self):
+        self.name = None
+
+    def get_name(self):
+        return self.name
+
+    def add_to_sausages(self):
+        Sausage.sausages.append(self.name)
+
+
+class Milky(Sausage):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Молочная'
+
+
+class Vegan_Sausage(Sausage):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Соевая'
+
+
+class Sause:
+    sauses = []
+
+    def __init__(self):
+        self.name = None
+
+    def get_name(self):
+        return self.name
+
+    def add_to_sauses(self):
+        Sause.sauses.append(self.name)
+
+
+class Mayo(Sause):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Майонез'
+
+
+class Ketchup(Sause):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Кетчуп'
+
+
+class Mustard(Sause):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Горчица'
+
+
+class Toppings:
+    toppings = []
+
+    def __init__(self):
+        self.name = None
+
+    def get_name(self):
+        return self.name
+
+    def add_to_toppings(self):
+        Toppings.toppings.append(self.name)
+
+
+class Onion(Toppings):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Лук'
+
+
+class Jalapeno(Toppings):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Халапеньо'
+
+
+class Pickles(Toppings):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Маринованные огурцы'
--- На проверку/PyHW31/view.py
+++ На проверку/PyHW31/view.py
@@ -1,1 +1,133 @@
 from model import *
+import copy
+
+
+class Recipes:
+    recipes = []
+
+    def __init__(self, hot_dog):
+        self.hot_dog = hot_dog
+
+    def add_recipe(self):
+        Recipes.recipes.append(self.hot_dog)
+
+    def get_recipes(self):
+        return Recipes.recipes
+
+
+class ShowNames(Recipes):
+    def __init__(self):
+        super().__init__(self)
+
+    @staticmethod
+    def show_names():
+        count = 0
+        for recipe in Recipes.recipes:
+            count += 1
+            for n, name in recipe.items():
+                if n == 'Название':
+                    print(f'{count} - {name}')
+
+
+class ShowRecipes(Recipes):
+    def __init__(self):
+        super().__init__(self)
+
+    @staticmethod
+    def show():
+        count = 0
+        for recipe in Recipes.recipes:
+            count += 1
+            print(f'\nРецепт №{count}:')
+            for ingredient, name in recipe.items():
+                print(f'\t{ingredient} - {name}')
+
+
+class ShowIngredients:
+    def __init__(self, list_of_ingredients):
+        self.list = list_of_ingredients
+    def show(self):
+        count = 0
+        for ingredient in self.list:
+            count += 1
+            print(f'{count} - {ingredient}')
+
+
+##menu
+
+
+standart_name = Standart()
+mexican_name = Mexican()
+vegan_name = Vegan()
+
+wheat = Wheat()
+rye = Rye()
+
+milky_sausage = Milky()
+vegan_sausage = Vegan_Sausage()
+
+mayonaise = Mayo()
+ketchup = Ketchup()
+mustard = Mustard()
+
+onion = Onion()
+jalapeno = Jalapeno()
+pickles = Pickles()
+
+def make_recipes():
+    recipe1 = HotDog(standart_name.get_name(), wheat.get_name(), milky_sausage.get_name(),
+                     ketchup.get_name(), onion.get_name())
+    standart = Recipes(recipe1.get_hot_dog())
+    standart.add_recipe()
+
+    recipe2 = HotDog(mexican_name.get_name(), wheat.get_name(), milky_sausage.get_name(),
+                     mustard.get_name(), jalapeno.get_name())
+    mexican = Recipes(recipe2.get_hot_dog())
+    mexican.add_recipe()
+
+    recipe3 = HotDog(vegan_name.get_name(), rye.get_name(), vegan_sausage.get_name(),
+                     ketchup.get_name(), pickles.get_name())
+    vegan = Recipes(recipe3.get_hot_dog())
+    vegan.add_recipe()
+make_recipes()
+
+def show_names():
+    ShowNames.show_names()
+
+def show_recipes():
+    ShowRecipes.show()
+
+def adding_to_lists():
+    standart_name.add_to_names()
+    mexican_name.add_to_names()
+    vegan_name.add_to_names()
+    wheat.add_to_buns()
+    rye.add_to_buns()
+    onion.add_to_toppings()
+    jalapeno.add_to_toppings()
+    pickles.add_to_toppings()
+    milky_sausage.add_to_sausages()
+    vegan_sausage.add_to_sausages()
+
+# def show_ingredients():
+#     list_names = RecipeName.names
+#     list_buns = Bun.buns
+
+
+def menu():
+    print(f'\nДобро пожаловать в киоск. Я знаю, вы хотите попробовать наши знаменитые хот-доги.\n'
+          f'Вы можете выбрать хот-дог из меню или создать свой.\n'
+          f'Наши хот-доги:\n')
+    show_names()
+    show_recipes()
+
+    choice = int(input('''\nВыберите, что хотите сделать.
+    1 - Выбрать хот-дог из меню,
+    2 - Создать свой.\n'''))
+
+    if choice == 2:
+        adding_to_lists()
+
+menu()
+
+
