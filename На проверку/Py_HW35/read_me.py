679546f615d8e48a3e1e40484d1f68b3977010b2
pokakaite <danilova.katherine02@gmail.com>
2024-05-30 16:54:35 +0500

    дз

--- На проверку/Py_HW35/read_me.py
+++ На проверку/Py_HW35/read_me.py
@@ -14,3 +14,47 @@
 # Реализуйте клиент — серверное приложение, позволяющее пользователям общаться в одном чате. Каждый
 # пользователь входит в чат под своим логином и паролем.
 # Сообщение, отправленное в чат видно всем пользователям чата.
+
+
+# import socket
+#
+# while True:
+#     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server.bind(('127.0.0.1', 9897))
+#     server.listen()
+#     client, address = server.accept()
+#     message = client.recv(1024).decode('utf-8')
+#     if message == 'quit':
+#         pass
+#     else:
+#         print(message)
+#     client.send(input('Server: ').encode('utf-8'))
+#
+#     client.close()
+#     server.close()
+#
+#
+#
+#
+# import socket
+#
+# while True:
+#     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     client.connect(('127.0.0.1', 9897))
+#
+#     client.send(input('Client: ').encode('utf-8'))
+#     message = client.recv(1024).decode('utf-8')
+#     if message == 'quit':
+#         pass
+#     else:
+#         print(message)
+#
+#     client.close()
+
+
+field = {}
+field.update({1: 'x'})
+print(field)
+field.update({2: 'o'})
+print(field)
+print(type(field))
--- На проверку/Py_HW35/Задание_1/client.py
+++ На проверку/Py_HW35/Задание_1/client.py
@@ -1,33 +1,12 @@
 import socket
-import json
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 9897))
 
-steps = []
-with open('file.json', 'r') as f:
-    data = f.read()
-    if steps == []:
-        pass
-    else:
-        steps.append(data)
+client_msg = input('Client: ')
+client.send(client_msg.encode('utf-8'))
+server_msg = client.recv(1024).decode('utf-8')
 
-
-
-
-flag = True
-while flag:
-    print('\nВы ходите ноликом.')
-    client.send(input('Ваш ход (Введите номер ячейки) - ').encode('utf-8'))
-    message = client.recv(1024).decode('utf-8')
-    if message == 'quit':
-        flag = False
-    else:
-        print()
-        sign = 'x'
-        steps.append({message: sign})
-        with open('file.json', 'a') as f:
-            json.dump({message: sign}, f)
-
+print(server_msg)
 
 client.close()
