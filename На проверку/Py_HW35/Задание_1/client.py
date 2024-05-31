99e396a9d3fdde7107b8daa24e2eb578ad6de4c4
pokakaite <danilova.katherine02@gmail.com>
2024-05-31 15:49:37 +0500

    дз

--- На проверку/Py_HW35/Задание_1/client.py
+++ На проверку/Py_HW35/Задание_1/client.py
@@ -1,12 +1,72 @@
 import socket
+import json
+
+with open('file.json', 'w') as f:
+    data = {"1": " ",
+            "2": " ",
+            "3": " ",
+            "4": " ",
+            "5": " ",
+            "6": " ",
+            "7": " ",
+            "8": " ",
+            "9": " "}
+    json.dump(data, f)
+
+
+def save_to_json(message, sign):
+    with open('file.json', 'r+') as f1:
+        data = json.load(f1)
+        data.update({message: sign})
+    with open('file.json', 'w') as f2:
+        json.dump(data, f2)
+
+
+def show_field():
+    with open('file.json', 'r') as f:
+        data = json.load(f)
+
+    return f'''\n {data['1']} | {data['2']} | {data['3']} 
+---+---+---
+ {data['4']} | {data['5']} | {data['6']}
+---+---+---
+ {data['7']} | {data['8']} | {data['9']}'''
+
+def show_win_or_lose(place_1, place_2, place_3):
+    with open('file.json', 'r') as f:
+        data = json.load(f)
+    if data[str(place_1)] == 'x' and data[str(place_1)] == data[str(place_2)] and data[str(place_1)] == data[str(place_3)]:
+        print('\nВЫ ПОБЕДИЛИ.')
+        print('Конец игры')
+        client.close()
+    elif data[str(place_1)] == 'o' and data[str(place_1)] == data[str(place_2)] and data[str(place_1)] == data[str(place_3)]:
+        print('\nВЫ ПРОИГРАЛИ.')
+        print('Конец игры')
+        client.close()
+
+
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 9897))
+print('Вы ходите крестиком.')
+print(show_field())
 
-client_msg = input('Client: ')
-client.send(client_msg.encode('utf-8'))
-server_msg = client.recv(1024).decode('utf-8')
-
-print(server_msg)
-
-client.close()
+while True:
+    client_msg = input('\nВаш ход (Введите номер ячейки): ')
+    save_to_json(client_msg, 'x')
+    print(show_field())
+    show_win_or_lose(1, 2, 3)
+    show_win_or_lose(1, 4, 7)
+    show_win_or_lose(1, 5, 9)
+    show_win_or_lose(2, 5, 8)
+    show_win_or_lose(4, 5, 6)
+    show_win_or_lose(3, 5, 7)
+    show_win_or_lose(3, 6, 9)
+    show_win_or_lose(7, 8, 9)
+    client.send(client_msg.encode('utf-8'))
+    message = client.recv(1024).decode('utf-8')
+    if message == 'quit':
+        client.close()
+    else:
+        print('\nХод противника:')
+        print(show_field())
--- На проверку/Py_HW35/Задание_1/server.py
+++ На проверку/Py_HW35/Задание_1/server.py
@@ -1,14 +1,61 @@
 import socket
+import json
+def save_to_json(message, sign):
+    with open('file.json', 'r+') as f1:
+        data = json.load(f1)
+        data.update({message: sign})
+    with open('file.json', 'w') as f2:
+        json.dump(data, f2)
+
+def show_field():
+    with open('file.json', 'r') as f:
+        data = json.load(f)
+
+    return f'''\n {data['1']} | {data['2']} | {data['3']} 
+---+---+---
+ {data['4']} | {data['5']} | {data['6']}
+---+---+---
+ {data['7']} | {data['8']} | {data['9']}'''
+
+def show_win_or_lose(place_1, place_2, place_3):
+    with open('file.json', 'r') as f:
+        data = json.load(f)
+    if data[str(place_1)] == 'o' and data[str(place_1)] == data[str(place_2)] and data[str(place_1)] == data[str(place_3)]:
+        print('\nВЫ ПОБЕДИЛИ.')
+        print('Конец игры')
+        client.close()
+        server.close()
+    elif data[str(place_1)] == 'x' and data[str(place_1)] == data[str(place_2)] and data[str(place_1)] == data[str(place_3)]:
+        print('\nВЫ ПРОИГРАЛИ.')
+        print('Конец игры')
+        client.close()
+        server.close()
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(('127.0.0.1', 9897))
 server.listen()
 client, address = server.accept()
-client_msg = client.recv(1024).decode('utf-8')
+print('Вы ходите ноликом.')
 
-print(client_msg)
-server_msg = input('Server: ')
-client.send(server_msg.encode('utf-8'))
+while True:
+    message = client.recv(1024).decode('utf-8')
+    if message == 'quit':
+        client.close()
+        server.close()
+    else:
+        print('\nХод противника:')
+        print(show_field())
+    server_msg = input('\nВаш ход (Введите номер ячейки): ')
+    save_to_json(server_msg, 'o')
+    print(show_field())
+    show_win_or_lose(1, 2, 3)
+    show_win_or_lose(1, 4, 7)
+    show_win_or_lose(1, 5, 9)
+    show_win_or_lose(2, 5, 8)
+    show_win_or_lose(4, 5, 6)
+    show_win_or_lose(3, 5, 7)
+    show_win_or_lose(3, 6, 9)
+    show_win_or_lose(7, 8, 9)
+    client.send(server_msg.encode('utf-8'))
 
-client.close()
-server.close()
