c67e0c8096b944ee70542caa9ff4a6b7979cfadb
pokakaite <danilova.katherine02@gmail.com>
2024-08-10 18:43:38 +0500

    'дз'

--- На проверку/Py_HW39/blog/writers/urls.py
+++ На проверку/Py_HW39/blog/writers/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', writers, name='writers'),
     path('Hemingway/', hemingway, name='Hemingway'),
     path('Shakespeare/', shakespeare, name='Shakespeare'),
-    path('Hemingway/<str:book>/', hemingway_book),
-    path('Shakespeare/<str:book>/', shakespeare_book),
+    path('Hemingway/<str:book>/', hemingway_book, name='Hemingway_book'),
+    path('Shakespeare/<str:book>/', shakespeare_book, name='Shakespeare_book'),
     path('<str:smth>/', smth)
 ]
