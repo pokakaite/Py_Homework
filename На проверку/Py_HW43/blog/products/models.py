f7c0d4030ac352bd7c94ec7d3dba4c3aac3e81f8
pokakaite <danilova.katherine02@gmail.com>
2024-08-29 13:39:01 +0500

    дз

--- На проверку/Py_HW43/blog/products/models.py
+++ На проверку/Py_HW43/blog/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from salesman.models import Salesman
 
 # Create your models here.
+
+class Products(models.Model):
+    name = models.CharField('Название', max_length=150, blank=True, null=True)
+    description = models.CharField('Описание', max_length=150, blank=True, null=True)
+    salesman = models.ForeignKey(Salesman, on_delete=models.CASCADE)
