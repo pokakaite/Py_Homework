d2c1d68e7082c6d36b22c7dbc05111a367fdd312
pokakaite <danilova.katherine02@gmail.com>
2024-08-30 16:00:44 +0500

    дз

---
+++ На проверку/Py_HW43/blog/customer/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField('Имя', max_length=150, blank=True)
+    last_name = models.CharField('Фамилия', max_length=150, blank=True)
+    number = models.IntegerField('Номер телефона', blank=True)
+    email = models.CharField('E-mail', max_length=150, blank=True)
+
+    def __str__(self):
+        return self.name
---
+++ На проверку/Py_HW43/blog/products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from salesman.models import Salesman
+
+# Create your models here.
+
+class Products(models.Model):
+    name = models.CharField('Название', max_length=150, blank=True, null=True)
+    description = models.CharField('Описание', max_length=150, blank=True, null=True)
+    price = models.IntegerField('Цена')
+    salesman = models.ForeignKey(Salesman, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
