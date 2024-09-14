d9adf9027ac15ad514810e296c68c7ba7378ec84
pokakaite <danilova.katherine02@gmail.com>
2024-09-14 15:54:27 +0500

    дз

--- На проверку/Py_HW44/blog/books/models.py
+++ На проверку/Py_HW44/blog/books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Books(models.Model):
+    book_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=150)
+    author = models.CharField(max_length=150)
+    year = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    publishment = models.CharField(max_length=150)
+    in_library = models.BooleanField()
+
+
+class BooksReaders(models.Model):
+    book_id = models.ForeignKey(on_delete=models.CASCADE)
--- На проверку/Py_HW44/blog/readers/models.py
+++ На проверку/Py_HW44/blog/readers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Readers(models.Model):
+    reader_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    number = models.ImageField(unique=True)
+    email = models.EmailField(unique=True)
+    date = models.DateField
