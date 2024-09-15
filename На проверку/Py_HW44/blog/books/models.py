88e4f37ea5518d403c5c51423bb98e9b66ca3574
pokakaite <danilova.katherine02@gmail.com>
2024-09-15 10:54:22 +0500

    дз

--- На проверку/Py_HW44/blog/books/models.py
+++ На проверку/Py_HW44/blog/books/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+from readers.models import Readers
 
 # Create your models here.
 
 class Books(models.Model):
-    book_id = models.IntegerField(unique=True)
+    book_id = models.IntegerField(unique=True, primary_key=True, null=False)
     name = models.CharField(max_length=150)
     author = models.CharField(max_length=150)
     year = models.IntegerField()
     genre = models.CharField(max_length=50)
     publishment = models.CharField(max_length=150)
-    in_library = models.BooleanField()
+    in_library = models.BooleanField(default=bool)
+    reader_id = models.ForeignKey(Readers, on_delete=models.CASCADE, blank=True, null=True)
 
-
-class BooksReaders(models.Model):
-    book_id = models.ForeignKey(on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.name
--- На проверку/Py_HW44/blog/readers/models.py
+++ На проверку/Py_HW44/blog/readers/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Readers(models.Model):
-    reader_id = models.IntegerField(unique=True)
+    reader_id = models.IntegerField(unique=True, primary_key=True, null=False)
     name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    number = models.ImageField(unique=True)
+    number = models.IntegerField(unique=True)
     email = models.EmailField(unique=True)
     date = models.DateField
+
+    def __str__(self) -> str:
+        return self.name
